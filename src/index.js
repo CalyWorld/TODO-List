38465bd58ce2e43705b508d1129aebb8be2e31ec
CalAnizoba <obinnaanizoba5@gmail.com>
2022-03-30 13:15:44 +0800

    index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import Homepage from "./Pages/Homepage";
+import DOMpage from "./Pages/DOMpage";
+import Logicpage from "./Pages/Logicpage"
+
+
+// const openform = ()=>{
+//     const taskbtn = document.querySelector(".taskbtn");
+//     taskbtn.addEventListener("click", ()=>{
+//         if(formContainer.style.display !== "none"){
+//             formContainer.style.display = "block";
+//         }else{
+//             formContainer.style.display = "block";
+//         }
+//     });
+// }
+
+// openform()
 
 
 Homepage();
+DOMpage();
+Logicpage();
