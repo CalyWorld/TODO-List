9202a3ee54dfaae2e729799766fc11785bf23582
CalAnizoba <obinnaanizoba5@gmail.com>
2022-03-31 17:38:13 +0800

    added logicPage file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import Homepage from "./Pages/Homepage";
 import DOMpage from "./Pages/DOMpage";
-import Logicpage from "./Pages/Logicpage"
+import Logicpage from "./Pages/Logicpage";
 
 
-// const openform = ()=>{
-//     const taskbtn = document.querySelector(".taskbtn");
-//     taskbtn.addEventListener("click", ()=>{
-//         if(formContainer.style.display !== "none"){
-//             formContainer.style.display = "block";
-//         }else{
-//             formContainer.style.display = "block";
-//         }
-//     });
-// }
-
-// openform()
 
 
 Homepage();
