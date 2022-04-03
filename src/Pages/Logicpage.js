acef6fbf4b4eb02311a05428b83fde900b3aff6f
CalAnizoba <obinnaanizoba5@gmail.com>
2022-04-03 22:12:10 +0800

    Added a delete button to delete card details from inboxDiv and list array

--- src/Pages/Logicpage.js
+++ src/Pages/Logicpage.js
@@ -1,5 +1,5 @@
 const list = [];
-let nextListId = 1;
+let nextListId = 0;
 class Todo {
     constructor(title, description, dueDate, priority, id) {
         this.title = title;
@@ -23,52 +23,64 @@ const addListToLibrary = () => {
 }
 
 const createCard = () => {
-    const titleName = document.getElementById("titlename").value;
-    const descriptionName = document.getElementById("description").value;
-    const dueDateNo = document.getElementById("dueDate").value;
-    const priorityId = document.getElementById("priority").value;
     const inboxDiv = document.getElementById("inboxDiv");
+    inboxDiv.textContent = "";
     const formContainer = document.getElementById("form-container");
-    const titleHolder = document.createElement("p");
-    const descriptionHolder = document.createElement("p");
-    const dueDateHolder = document.createElement("p");
-    const priorityHolder = document.createElement("p");
-    const inbox = document.createElement("div");
-    const inboxCardDiv = document.createElement("div");
-    const inboxExposedDiv = document.createElement("div");
-    const inboxDeletebtn = document.createElement("button");
-    inboxExposedDiv.setAttribute("id", list.length);
-    inbox.classList.add("inbox-Collapse");
-    inboxCardDiv.classList.add("inbox-Card");
-    inboxExposedDiv.classList.add("inbox-Exposed");
-    titleHolder.textContent = `Title: ${titleName}`;
-    descriptionHolder.textContent = `Description: ${descriptionName}`;
-    dueDateHolder.textContent = `Due Date: ${dueDateNo}`;
-    priorityHolder.textContent = `Priority: ${priorityId}`;
-    inboxDeletebtn.textContent = "Delete";
-    inboxExposedDiv.append(titleName);
-    inboxExposedDiv.append(inboxDeletebtn);
-    inboxCardDiv.append(inboxExposedDiv);
-    inbox.append(titleHolder);
-    inbox.append(descriptionHolder);
-    inbox.append(dueDateHolder);
-    inbox.append(priorityHolder);
-    inboxCardDiv.append(inbox);
-    inboxDiv.append(inboxCardDiv);
-    formContainer.style.display = "none";
-    inboxDiv.style.display = "block";
+    for (let i of list) {
+        const titleName = document.getElementById("titlename").value;
+        const descriptionName = document.getElementById("description").value;
+        const dueDateNo = document.getElementById("dueDate").value;
+        const priorityId = document.getElementById("priority").value;
+        const titleHolder = document.createElement("p");
+        const descriptionHolder = document.createElement("p");
+        const dueDateHolder = document.createElement("p");
+        const priorityHolder = document.createElement("p");
+        const inbox = document.createElement("div");
+        const inboxCardDiv = document.createElement("div");
+        const inboxExposedDiv = document.createElement("div");
+        const inboxDeletebtn = document.createElement("button");
+        inboxCardDiv.setAttribute("id", `${i.id}`);
+        inboxDeletebtn.setAttribute("id", `${i.id}`);
+        inbox.classList.add("inbox-Collapse");
+        inboxCardDiv.classList.add("inbox-Card");
+        inboxExposedDiv.classList.add("inbox-Exposed");
+        titleHolder.textContent = `Title: ${titleName}`;
+        descriptionHolder.textContent = `Description: ${descriptionName}`;
+        dueDateHolder.textContent = `Due Date: ${dueDateNo}`;
+        priorityHolder.textContent = `Priority: ${priorityId}`;
+        inboxDeletebtn.textContent = "Delete";
+        inboxExposedDiv.textContent = `${i.title}`;
+        inboxExposedDiv.append(inboxDeletebtn);
+        inboxCardDiv.append(inboxExposedDiv);
+        inbox.append(titleHolder);
+        inbox.append(descriptionHolder);
+        inbox.append(dueDateHolder);
+        inbox.append(priorityHolder);
+        inboxCardDiv.append(inbox);
+        inboxDiv.append(inboxCardDiv);
+        formContainer.style.display = "none";
+        inboxDiv.style.display = "block";
 
-    inboxExposedDiv.addEventListener("click", e => {
-        if (e.target.className === "inbox-Exposed") {
-            if (inbox.style.display != "none") {
-                inbox.style.display = "none";
-            } else {
-                inbox.style.display = "flex";
+        inboxExposedDiv.addEventListener("click", e => {
+            if (e.target.className === "inbox-Exposed") {
+                if (inbox.style.display != "none") {
+                    inbox.style.display = "none";
+                } else {
+                    inbox.style.display = "flex";
+                }
             }
-        }
-    });
+        });
 
-};
+        inboxDeletebtn.addEventListener("click", e => {
+            list.splice(list.findIndex(current=>{
+                return current.id === i.id;
+            }), 1);
+            inboxDiv.removeChild(inboxCardDiv);
+            console.log(list);
+        });
+
+    };
+}
 
 const submitForm = () => {
     const submitbtn = document.querySelector("#submitbtn");
