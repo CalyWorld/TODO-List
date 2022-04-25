0195a504be8f8817b16e8f8a2d6e7c9cf6ea1955
CalAnizoba <obinnaanizoba5@gmail.com>
2022-04-25 14:22:52 +0800

    fixed movebtn by cloning projectlist

--- src/Pages/Logicpage.js
+++ src/Pages/Logicpage.js
@@ -39,7 +39,7 @@ Todo.prototype.toggleStatus = function () {
 const createCard = () => {
     const formContainer = document.getElementById("form-container");
     const inboxDiv = document.getElementById("inboxDiv");
-    const projectHolder = document.getElementById("rightProjectHolder")
+    const projectHolder = document.getElementById("rightProjectHolder");
     const editFormContainer = document.querySelector(".editform-container")
     const updatebtn = document.querySelector(".updatebtn");
     const projectlist = document.getElementById("projectlist");
@@ -47,19 +47,19 @@ const createCard = () => {
     console.log(index);
     var projectDiv = arrayOfProjects[index];
     console.log(projectDiv);
-    for (let i of list) {
+    for (var i of list) {
         const titleHolder = document.createElement("p");
         const descriptionHolder = document.createElement("p");
         const dueDateHolder = document.createElement("p");
         const priorityHolder = document.createElement("p");
-        const inbox = document.createElement("div");
+        var inbox = document.createElement("div");
         var inboxCardDiv = document.createElement("div");
-        const inboxExposedDiv = document.createElement("div");
-        const inboxExposedBtnDiv = document.createElement("div");
-        const inboxDeletebtn = document.createElement("button");
-        const readbtn = document.createElement("button");
-        const movebtn = document.createElement("button");
-        const editbtn = document.createElement("button");
+        var inboxExposedDiv = document.createElement("div");
+        var inboxExposedBtnDiv = document.createElement("div");
+        var inboxDeletebtn = document.createElement("button");
+        var readbtn = document.createElement("button");
+        var movebtn = document.createElement("button");
+        var editbtn = document.createElement("button");
 
         inboxCardDiv.setAttribute("id", `${i.id}`);
         inboxDeletebtn.setAttribute("id", `${i.id}`);
@@ -97,122 +97,124 @@ const createCard = () => {
         inboxCardDiv.append(inbox);
 
         formContainer.style.display = "none";
-
-
-        inboxExposedDiv.addEventListener("click", e => {
-            if (e.target.className === "inbox-Exposed") {
-                if (inbox.style.display === "none") {
-                    inbox.style.display = "flex";
-                } else if (inbox.style.display = "none") {
-                    inbox.style.display = "none";
-                }
-            }
-        });
-
-        inboxDeletebtn.addEventListener("click", () => {
-            list.splice(list.findIndex(current => {
-                return current.id === i.id;
-            }), 1);
-            inboxCardDiv.remove();
-            console.log(list);
-        });
-
-        readbtn.addEventListener("click", () => {
-            list[
-                list.findIndex((current) => {
-                    return current.id === i.id;
-                })].toggleStatus();
-            readbtn.textContent = `Read: ${i.read}`;
-            console.log(list);
-        });
-
-        editbtn.addEventListener("click", (e) => {
-            let clickedlist = e.target;
-            let index = '';
-            index = clickedlist.getAttribute("id");
-            console.log(index);
-            if (e.target.id === index) {
-                document.getElementById("editTitlename").value = `${i.title}`;
-                document.getElementById("editdescription").value = `${i.description}`;
-                document.getElementById("editdueDate").value = `${i.dueDate}`;
-                document.getElementById("editpriority").value = `${i.priority}`;
-                document.getElementById("editread").checked = `${i.read}`;
-            }
-            console.log(list);
-            updatebtn.setAttribute("id", index);
-            editFormContainer.style.display = "block";
-        });
-
-        updatebtn.addEventListener("click", (e) => {
-            const editTitleName = document.getElementById("editTitlename").value;
-            const editdescriptionName = document.getElementById("editdescription").value;
-            const editdueDateNo = document.getElementById("editdueDate").value;
-            const editpriorityId = document.getElementById("editpriority").value;
-            let editRead = document.getElementById("editread").checked;
-            let objIndex = parseInt(e.target.id);
-            list[objIndex].title = editTitleName;
-            list[objIndex].description = editdescriptionName;
-            list[objIndex].dueDate = editdueDateNo;
-            list[objIndex].priority = editpriorityId;
-            list[objIndex].read = editRead;
-            createCard();
-            console.log(list);
-            editFormContainer.style.display = "none";
-        });
-
-        movebtn.addEventListener("click", () =>{
-            const moveForm = document.createElement("form");
-            const projectLabel = document.createElement("label");
-            const moveSubmitbtnDiv = document.createElement("div");
-            const moveSubmitbtn = document.createElement("input");
-            const cancelSubmitbtn = document.createElement("input");
-
-            moveSubmitbtnDiv.append(moveSubmitbtn);
-            moveSubmitbtnDiv.append(cancelSubmitbtn);
-
-            moveSubmitbtnDiv.classList.add("moveSubmitbtnDiv");
-            moveSubmitbtn.setAttribute("type", "button");
-            moveSubmitbtn.setAttribute("id", `${i.id}`);
-            moveSubmitbtn.setAttribute("value", "Move Button");
-            moveSubmitbtn.classList.add("moveSubmitbtn");
-            cancelSubmitbtn.setAttribute("type", "button");
-            cancelSubmitbtn.setAttribute("id", `${i.id}`);
-            cancelSubmitbtn.setAttribute("value", "Cancel");
-            cancelSubmitbtn.classList.add("cancelSubmitbtn");
-
-            moveForm.classList.add("moveform");
-            projectLabel.classList.add("moveLabel");
-            projectLabel.textContent = "Project:";
-
-            moveForm.append(projectLabel);
-            //problem
-            moveForm.append(projectlist);
-            moveForm.append(moveSubmitbtnDiv);
-
-            moveForm.style.display = "block";
-            document.body.append(moveForm);
-
-            moveSubmitbtn.addEventListener("click", () => {
-                moveSelectIndex = projectlist.selectedIndex;
-                moveProjectDiv = arrayOfProjects[moveSelectIndex];
-                console.log(moveSelectIndex);
-                if (moveSelectIndex === 0){
-                    inboxDiv.append(inboxCardDiv);
-                    console.log(moveSelectIndex);
-                }else if(moveSelectIndex === parseInt(moveProjectDiv.getAttribute("data-data-Id"))){
-                    console.log(moveSelectIndex);
-                    projectDiv.append(inboxCardDiv);
-                }
-                moveForm.remove();
-            });
-
-            cancelSubmitbtn.addEventListener("click", () => {
-                moveForm.style.display = "none";
-            });
-        });
-
-
     }
+
+    inboxExposedDiv.addEventListener("click", e => {
+        if (e.target.className === "inbox-Exposed") {
+            if (inbox.style.display === "none") {
+                inbox.style.display = "flex";
+            } else if (inbox.style.display = "none") {
+                inbox.style.display = "none";
+            }
+        }
+    });
+
+    inboxDeletebtn.addEventListener("click", () => {
+        list.splice(list.findIndex(current => {
+            return current.id === i.id;
+        }), 1);
+        inboxCardDiv.remove();
+        console.log(list);
+    });
+
+    readbtn.addEventListener("click", () => {
+        list[
+            list.findIndex((current) => {
+                return current.id === i.id;
+            })].toggleStatus();
+        readbtn.textContent = `Read: ${i.read}`;
+        console.log(list);
+    });
+
+    editbtn.addEventListener("click", (e) => {
+        let clickedlist = e.target;
+        let index = '';
+        index = clickedlist.getAttribute("id");
+        console.log(index);
+        if (e.target.id === index) {
+            document.getElementById("editTitlename").value = `${i.title}`;
+            document.getElementById("editdescription").value = `${i.description}`;
+            document.getElementById("editdueDate").value = `${i.dueDate}`;
+            document.getElementById("editpriority").value = `${i.priority}`;
+            document.getElementById("editread").checked = `${i.read}`;
+        }
+        console.log(list);
+        updatebtn.setAttribute("id", index);
+        editFormContainer.style.display = "block";
+    });
+
+    updatebtn.addEventListener("click", (e) => {
+        const editTitleName = document.getElementById("editTitlename").value;
+        const editdescriptionName = document.getElementById("editdescription").value;
+        const editdueDateNo = document.getElementById("editdueDate").value;
+        const editpriorityId = document.getElementById("editpriority").value;
+        let editRead = document.getElementById("editread").checked;
+        let objIndex = parseInt(e.target.id);
+        list[objIndex].title = editTitleName;
+        list[objIndex].description = editdescriptionName;
+        list[objIndex].dueDate = editdueDateNo;
+        list[objIndex].priority = editpriorityId;
+        list[objIndex].read = editRead;
+        console.log(list);
+        editFormContainer.style.display = "none";
+    });
+
+    movebtn.addEventListener("click", () => {
+        const projectlist = document.getElementById("projectlist");
+        const moveProjectlist = projectlist.cloneNode(true);
+        const moveForm = document.createElement("form");
+        const projectLabel = document.createElement("label");
+        const moveSubmitbtnDiv = document.createElement("div");
+        const moveSubmitbtn = document.createElement("input");
+        const cancelSubmitbtn = document.createElement("input");
+
+        moveSubmitbtnDiv.append(moveSubmitbtn);
+        moveSubmitbtnDiv.append(cancelSubmitbtn);
+
+        moveSubmitbtnDiv.classList.add("moveSubmitbtnDiv");
+        moveSubmitbtn.setAttribute("type", "button");
+        moveSubmitbtn.setAttribute("id", `${i.id}`);
+        moveSubmitbtn.setAttribute("value", "Move Button");
+        moveSubmitbtn.classList.add("moveSubmitbtn");
+        cancelSubmitbtn.setAttribute("type", "button");
+        cancelSubmitbtn.setAttribute("id", `${i.id}`);
+        cancelSubmitbtn.setAttribute("value", "Cancel");
+        cancelSubmitbtn.classList.add("cancelSubmitbtn");
+
+        moveForm.classList.add("moveform");
+        projectLabel.classList.add("moveLabel");
+        projectLabel.textContent = "Project:";
+
+        moveForm.append(projectLabel);
+        moveForm.append(moveProjectlist);
+        moveForm.append(moveSubmitbtnDiv);
+
+        moveForm.style.display = "block";
+        document.body.append(moveForm);
+
+        moveSubmitbtn.addEventListener("click", () => {
+            moveSelectIndex = moveProjectlist.selectedIndex;
+            moveProjectDiv = arrayOfProjects[moveSelectIndex];
+            console.log(moveSelectIndex);
+            if (moveSelectIndex === 0) {
+                inboxDiv.append(inboxCardDiv);
+                console.log(moveSelectIndex);
+            } else if (moveSelectIndex === parseInt(moveProjectDiv.getAttribute("data-data-Id"))) {
+                console.log(moveSelectIndex);
+                moveProjectDiv.append(inboxCardDiv);
+            }
+
+            moveForm.remove();
+
+        });
+
+        cancelSubmitbtn.addEventListener("click", () => {
+            moveForm.style.display = "none";
+        });
+    });
+
+
+
 
     if (index === 0) {
         inboxDiv.append(inboxCardDiv);
@@ -230,8 +232,6 @@ const submitForm = () => {
     const submitbtn = document.querySelector("#submitbtn");
     submitbtn.addEventListener("click", addListToLibrary);
 };
-
-
 
 const Logicpage = () => {
     submitForm();
